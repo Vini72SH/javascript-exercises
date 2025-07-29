4d0ac8100c52a4419c85ad4b27a5a035bf715485
Vinicius <viniciusjere@outlook.com>
2025-07-29 17:55:44 -0300

    08 - Solved

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+    let filterArr = [];
+
+    let size = arr.length;
+    for (let i = 0; i < size; ++i) {
+        if (!(remove.includes(arr[i]))) {
+            filterArr.push(arr[i]);
+        }
+    }
+
+    return filterArr;
 };
 
 // Do not edit below this line
