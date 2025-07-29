bc62868ea627017ac045a58166ce348ba1568f81
Vinicius <viniciusjere@outlook.com>
2025-07-29 17:40:12 -0300

    07 - Solved

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(reverse) {
+    let r = '';
+    let size = String(reverse).length;
 
+    let i;
+    for (i = size - 1; i >= 0; i--) {
+        let char = reverse[i];
+        r = r + char;
+    }
+
+    return r;
 };
 
 // Do not edit below this line
