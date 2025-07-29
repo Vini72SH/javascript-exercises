118e13f80ddd899cf5f656ea0aa5a56b96de019d
Vinicius <viniciusjere@outlook.com>
2025-07-29 18:22:49 -0300

    10 - Solved

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let div4 = (year % 4 == 0);
+    let div100 = (year % 100 == 0);
+    let div400 = (year % 400 == 0);
 
+    if ((div400) || (div4 && !(div100))) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
