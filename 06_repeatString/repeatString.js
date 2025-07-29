52b769a3b51213af820dbb4f057fafa675f95762
Vinicius <viniciusjere@outlook.com>
2025-07-29 17:29:41 -0300

    06 - Solved

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(repeat, times) {
+    if (times < 0) {
+        return 'ERROR';
+    }
 
+    if (times === 0) {
+        return '';
+    }
+
+    let sub = String(repeat);
+    let s = String(repeat);
+
+    for (let i = 1; i < times; ++i) {
+        s = s + sub;
+    }
+
+    return s;
 };
 
 // Do not edit below this line
