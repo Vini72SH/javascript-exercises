2dcbd82273722eb044b0383479f59990922bcb8b
Vinicius <viniciusjere@outlook.com>
2025-07-29 18:14:36 -0300

    09 - Solved

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
 
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
+
+    let min, max;
+
+    if (a < b) {
+        min = a;
+        max = b;
+    } else {
+        min = b;
+        max = a;
+    }
+
+    let result = ((max + min) * (max - min + 1)) / 2;
+    return result;
 };
 
 // Do not edit below this line
