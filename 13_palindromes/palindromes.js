ef2d94be0abf3d96213a11a7cf5e803e9e47f0a9
Vinicius <viniciusjere@outlook.com>
2025-08-01 14:47:19 -0300

    13 - Solved

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+const palindromes = function(words) {
+    let i, j;
+    let s = words.toLowerCase()
+                .split('')
+                .filter((character) => alphanumerical.includes(character))
+                .join('');
+    let palindrome = true;
+
+    for (i = 0; i < s.length / 2; ++i) {
+        j = s.length - (i + 1);
+        if (s[i] !== s[j]) {
+            palindrome = false;
+            break;
+        }
+    }
+
+    return palindrome;
 };
 
 // Do not edit below this line
