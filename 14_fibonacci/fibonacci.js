b32b0fd90c83c13f069643e528a2206c1ed77bab
Vinicius <viniciusjere@outlook.com>
2025-08-01 14:54:52 -0300

    14 - Solved

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n !== 'number') {
+        n = parseInt(n);
+    }
 
+    if (n < 0) {
+        return 'OOPS';
+    }
+
+    if (n <= 1) {
+        return n;
+    }
+
+    let penult, ult, fib, cont;
+    penult = 0;
+    ult = 1;
+    cont = 1;
+    while (cont < n) {
+        fib = ult + penult;
+        penult = ult;
+        ult = fib;
+        cont++;
+    }
+
+    return fib;
 };
 
 // Do not edit below this line
