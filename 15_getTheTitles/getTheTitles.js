a529ac139d32a5c18d01718b06496527273d0c28
Vinicius <viniciusjere@outlook.com>
2025-08-01 15:01:37 -0300

    15 - Solved

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(objects) {
+    let i;
+    let titles = [];
 
+    for (i = 0; i < objects.length; ++i) {
+        titles.push(objects[i].title);
+    }
+
+    return titles;
 };
 
 // Do not edit below this line
