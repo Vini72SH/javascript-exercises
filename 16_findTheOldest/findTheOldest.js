2020ac595e6bba03c6779edafca732e6d5da1ae2
Vinicius <viniciusjere@outlook.com>
2025-08-01 15:14:21 -0300

    16 - Solved

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+function getAge(person) {
+    const death =
+        person.yearOfDeath ? person.yearOfDeath : new Date().getFullYear();
+    return death - person.yearOfBirth;
+};
 
+
+const findTheOldest = function(peoples) {
+    let i, currentAge;
+    let oldest = peoples[0];
+    let oldestAge = getAge(peoples[0]);
+
+    for (i = 1; i < peoples.length; ++i) {
+        currentAge = getAge(peoples[i]);
+        if (currentAge > oldestAge) {
+            oldest = peoples[i];
+            oldestAge = currentAge;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
